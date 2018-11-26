fbfd900f54c808765b8236e8453d85b04d67a250
Franko <1574989439@qq.com>
2018-11-26 16:16:17 +0800

    歌曲播放时光碟转动

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,39 +1,43 @@
 {
   let view = {//添加audio标签与播放暂停按钮
-    el:'#app',
-    template:`
-      <audio src={{url}}></audio>
-      <div>
-        <button class="play">播放音乐</button>
-        <button class="pause">暂停音乐</button>
-      </div>
-    `,
-    render(data){//接受到歌曲信息进行渲染
-      $(this.el).html(this.template.replace('{{url}}',data.url))//将url置入audio标签
+    el: '#app',
+    render(data, status) {//对页面进行渲染
+      let song = data
+      $(this.el).css('background-image', `url(${song.cover})`)//替换背景图片
+      $(this.el).find('img.cover').attr('src', song.cover)//替换光碟封面
+      if ($(this.el).find('audio').attr('src') !== song.url) {//如果歌曲已经存在，就不重新渲染
+        $(this.el).find('audio').attr('src', song.url)
+      }
+      if (status === 'playing') {//根据歌曲播放状态判断光碟是否转动
+        $(this.el).find('.disc-container').addClass('playing')
+      } else {
+        $(this.el).find('.disc-container').removeClass('playing')
+      }
     },
-    play(){//播放事件
-      let audio = $(this.el).find('audio')[0]
-      audio.play()
+    play() {//播放歌曲
+      $(this.el).find('audio')[0].play()
     },
-    pause(){//暂停事件
-      let audio = $(this.el).find('audio')[0]
-      audio.pause()
+    pause() {//暂停歌曲
+      $(this.el).find('audio')[0].pause()
     }
   }
   let model = {
-    data:{
-      id:'',
-      name:'',
-      singer:'',
-      url:''
+    data: {
+      song: {//歌曲信息
+        id: '',
+        name: '',
+        singer: '',
+        url: ''
+      },
+      status: 'paused'//歌曲状态
     },
-    setId(id){
-      this.data.id = id
+    setId(id) {
+      this.data.song.id = id
     },
-    get(){//从数据库中根据传来的id进行查询
+    get() {//从数据库中根据传来的id进行查询
       var query = new AV.Query('Song')
-      return query.get(this.data.id).then((song)=>{//将获取到的数据song存入model中
-        Object.assign(this.data,song.attributes)
+      return query.get(this.data.song.id).then((song) => {//将获取到的数据song存入model中
+        Object.assign(this.data.song, song.attributes)
         return song//返回获取到的数据
       })
     }
@@ -44,17 +48,21 @@
       this.model = model
       let id = this.getSongId()//获取查询参数中的歌曲id
       this.model.setId(id)//将id存入model中
-      this.model.get().then(()=>{//根据获取到的id查询歌曲信息，并将得到的信息存入model
-        this.view.render(this.model.data)//接受歌曲信息，进行渲染
+      this.model.get().then(() => {//根据获取到的id查询歌曲信息，并将得到的信息存入model
+        this.view.render(this.model.data.song, this.model.data.status)//接受歌曲信息，进行渲染
       })
       this.bindEvents()
     },
-    bindEvents(){//绑定事件
-      $(this.view.el).on('click','.play',()=>{//点击按钮播放音乐
-        this.view.play()
+    bindEvents() {//绑定事件
+      $(this.view.el).on('click', '.icon-play', () => {//点击播放按钮
+        this.model.data.status = 'playing'//状态切换
+        this.view.render(this.model.data.song, this.model.data.status)//渲染页面
+        this.view.play()//播放歌曲
       })
-      $(this.view.el).on('click','.pause',()=>{//点击按钮暂停音乐
-        this.view.pause()
+      $(this.view.el).on('click', '.icon-pause', () => {//点击暂停按钮
+        this.model.data.status = 'paused'//状态切换
+        this.view.render(this.model.data.song, this.model.data.status)//渲染页面
+        this.view.pause()//暂停歌曲
       })
     },
     getSongId() {
@@ -78,5 +86,5 @@
       return id//返回id，此时id即为从查询参数中取出的值
     }
   }
-  controller.init(view,model)
+  controller.init(view, model)
 }
